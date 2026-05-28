b7e1b0d4843b7d597285b2279e003756898960a5
meyroneinc <mmeyroneinc@gmail.com>
2026-05-28 21:40:06 +0200

    Change hoverlabel bgcolor from #1f0207 to #000000

--- test.py
+++ test.py
@@ -197,7 +197,7 @@ def show_class_summary(dataframe):
                     font=dict(color="#ffffff", size=12)
                 ),
                 hoverlabel=dict(
-                    bgcolor="#1f0207",
+                    bgcolor="#000000",
                     font_size=14,
                     font_color="#ffffff"
                 )
